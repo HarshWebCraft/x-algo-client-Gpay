baa25261a256bac1153d8522d832d7e8e135d612
HarshWebCraft <harshwebdev26@gmail.com>
2024-07-02 15:19:45 +0530

    harsh

---
+++ src/components/MyWallet.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navbar from './Navbar'
+import './myWallet.css'
+import './button.scss'
+
+const MyWallet = () => {
+  return (
+    <>
+        <div className='container'>
+            <Navbar/>
+            <div className={`${localStorage.getItem('theme') === "light-theme" ? 'thaksn' : 'rgvba'}`}>
+                <h2 className='oquen'>My balance</h2>
+                <h2 className='fund'>$0.0000</h2>
+                <div className='with-dipo-button'>
+                    <a href="#" class="qscavn">Deposit</a>
+                    <a href="#" class="qscavn2">Withdraw</a>
+                </div>
+            </div>
+         </div>
+    </>
+  )
+}
+
+export default MyWallet
