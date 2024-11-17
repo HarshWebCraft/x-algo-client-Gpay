b9431d5a66c1ec4ee651bf1164bed3ce69dce48e
HarshTechCraft <harshdvadhavana26@gmail.com>
2024-11-17 12:25:53 +0530

    url chnage

--- src/URL/url.js
+++ src/URL/url.js
@@ -1,4 +1,5 @@
 module.exports = {
   // ProductionUrl: "https://api.xalgos.in",
-  ProductionUrl: "https://x-algo-gpay.onrender.com",
+  // ProductionUrl: "https://x-algo-gpay.onrender.com",
+  ProductionUrl: "https://oyster-app-4y3eb.ondigitalocean.app/",
 };
